4490e4ebe4226424025638b6782759597e2700ee
ASA <asa.kwarnmark@gmail.com>
2017-01-23 14:58:08 +0100

    Initial Commit

---
+++ Labb3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb3
+//
+//  Created by Asa Kwarnmark on 2017-01-23.
+//  Copyright © 2017 Åsa Kwarnmark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Labb3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb3
+//
+//  Created by Asa Kwarnmark on 2017-01-23.
+//  Copyright © 2017 Åsa Kwarnmark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
